d845095af2124f5784699f9a8ca02b97435b8a05
Liadrinz <435438602@qq.com>
2020-12-17 23:02:26 +0800

    弃用redis，并且对从sharedb中取出的文档id调用rest接口进行查询

--- src/tree.js
+++ src/tree.js
@@ -1,16 +1,12 @@
 const config = require('../server.config');
 const WebSocketJSONStream = require('@teamwork/websocket-json-stream');
-const redis = require('redis');
-const { sortTree } = require('./utils');
+const { sortTree, getDocDetail } = require('./utils');
 const { FileTree } = require('./file/FileTree');
 
 // 客户端引用计数
 const clients = {};
-// Redis
-const redisClient = redis.createClient(config.redis.port, config.redis.host);
-redisClient.auth(config.redis.auth);
 
-function tree(backend, connection, ws, type, userId) {
+async function tree(backend, connection, ws, type, userId, token) {
     if (clients[userId] === undefined) {
         clients[userId] = 0;
     }
@@ -30,40 +26,27 @@ function tree(backend, connection, ws, type, userId) {
             console.log(`[Loading] memory missing, create new ${type} tree of user ${userId}`);
             let root = {
                 children: {
-                    "算法": {
-                        id: 1,
+                    "0": {
                         label: "算法",
-                        creator: 1,
-                        collaborators: null,
                         show: false,
                         children: {
-                            "动态规划": { id: 2, label: "动态规划", type: "rich-text", creator: 1, collaborators: [2, 3] },
-                            "二叉树": { id: 3, label: "二叉树", type: "rich-text", creator: 1, collaborators: [2, 3] },
-                        }
-                    },
-                    "计算机网络": {
-                        id: 4,
-                        label: "计算机网络",
-                        creator: 1,
-                        collaborators: null,
-                        show: false,
-                        children: {
-                            "七层协议": {
-                                id: 7, label: "七层协议", creator: 1, collaborators: null, show: false, children: {
-                                    "TCP协议": { id: 5, label: "TCP协议", type: "rich-text", creator: 1, collaborators: [2, 3] },
-                                    "HTTP协议": { id: 6, label: "HTTP协议", type: "rich-text", creator: 1, collaborators: [2, 3] },
-                                }
-                            }
+                            "0": { id: 1 },
                         }
                     }
                 }
             }
-            sortTree(root);
-            rootTree = new FileTree(root);
-            doc.create(rootTree, function () {
+            getDocDetail(root.children, token).then(() => {
+                sortTree(root);
+                let rootTree = new FileTree(root);
+                doc.submitOp({
+                    p: [],
+                    oi: rootTree
+                });
+            })
+            doc.create({}, function() {
                 console.log(`[Loading] new ${type} tree of user ${userId} created`);
                 backend.listen(new WebSocketJSONStream(ws));
-            });
+            })
         }
     });
     ws.on('error', function (err) {
