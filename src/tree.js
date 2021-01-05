73250e69e6daaafcc8e70f834243b3ef9bfc082c
liadrinz <liadrinz>
2021-01-05 17:20:39 +0800

    文件树对接后端

--- src/tree.js
+++ src/tree.js
@@ -2,15 +2,22 @@ const config = require('../server.config');
 const WebSocketJSONStream = require('@teamwork/websocket-json-stream');
 const { sortTree, getDocDetail } = require('./utils');
 const { FileTree } = require('./file/FileTree');
+const { getDocTree, saveDocTree } = require("./dao/DocTree");
 
 // 客户端引用计数
 const clients = {};
+const taskRefs = {};
 
 async function tree(backend, connection, ws, type, userId, token) {
     if (clients[userId] === undefined) {
         clients[userId] = 0;
     }
     ++clients[userId];
+    if (taskRefs[userId]) {
+        clearTimeout(taskRefs[userId]);
+        delete taskRefs[userId];
+        console.log(`[Canceled] saving doc tree ${userId} to db is canceled`);
+    }
     // 从ShareDB加载树
     var doc = connection.get('tree-' + type, '' + userId);
     console.log(`[Loading] loading ${type} tree of user ${userId} from memory`);
@@ -18,49 +25,42 @@ async function tree(backend, connection, ws, type, userId, token) {
         if (err) {
             throw err;
         }
-        if (doc.type !== null) {
-            console.log(`[Loading] ${type} tree of user ${userId} loaded from memory`);
-            backend.listen(new WebSocketJSONStream(ws));
-        } else {
-            // 新建树
-            console.log(`[Loading] memory missing, create new ${type} tree of user ${userId}`);
-            let root = {
-                children: {
-                    // "算法": {
-                    //     label: "算法",
-                    //     show: false,
-                    //     children: {
-                    //         "计算机网络": {
-                    //             label: "计算机网络",
-                    //             show: false,
-                    //             children: {
-                    //                 "3": { id: 3 },
-                    //             }
-                    //         },
-                    //         "1": { id: 1 },
-                    //     }
-                    // },
-                }
-            }
+        // 新建树
+        var rootTree = {};
+        console.log(`[Loading] memory missing, create new ${type} tree of user ${userId}`);
+        getDocTree(userId, token).then((resp) => {
+            let root = resp.data.data.content;
+            root = JSON.parse(root);
             sortTree(root);
+            return root;
+        }).then((root) => {
             getDocDetail(root.children, token).then(() => {
-                let rootTree = new FileTree(root);
+                rootTree = new FileTree(root);
                 doc.submitOp({
                     p: [],
                     oi: rootTree
                 });
             })
-            doc.create({}, function() {
-                console.log(`[Loading] new ${type} tree of user ${userId} created`);
-                backend.listen(new WebSocketJSONStream(ws));
-            })
-        }
+        })
+        doc.create(rootTree, function () {
+            console.log(`[Loading] new ${type} tree of user ${userId} created`);
+            backend.listen(new WebSocketJSONStream(ws));
+        })
     });
     ws.on('error', function (err) {
         throw err;
     });
     ws.on('close', function () {
         --clients[userId];
+        console.log(`[Saving] saving doc tree ${userId} to db`);
+        doc.fetch((err) => {
+            if (err) throw err;
+            saveDocTree(userId, JSON.stringify(doc.data.root), token).then((resp) => {
+                console.log(`[Saved] doc tree ${userId} saved to db`);
+            }).catch((err) => {
+                console.err(`[Saving Failed] doc tree ${userId} failed saving to db`);
+            })
+        })
     })
 }
 
