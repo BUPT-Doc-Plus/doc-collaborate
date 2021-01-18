f8262657b3424380e2568e757bbb00d7d7684982
liadrinz <liadrinz>
2021-01-18 23:40:22 +0800

    建立id索引

--- src/file/FileTree.js
+++ src/file/FileTree.js
@@ -2,8 +2,12 @@ const { FileNode } = require("./FileNode");
 
 class FileTree {
     constructor(data) {
+        this.idPath = {};
         this.indices = {};
         this.root = new FileNode(data, "/", (node) => {
+            if (node.id) {
+                this.idPath[node.id] = node.path;
+            }
             this.indices[node.path] = node;
         });
     }
