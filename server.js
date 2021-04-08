fba2547d9ddcbd4d7e7c8a8579e6b10d56389a0c
unknown <>
2021-04-08 21:09:01 +0800

    0408

--- server.js
+++ server.js
@@ -35,6 +35,19 @@ startServer();
 function startServer() {
     // 创建express服务器
     var app = express();
+    // 允许跨域
+    app.all('*', function(req, res, next) {
+        console.log(req.headers.origin)
+        console.log(req.environ)
+        res.header("Access-Control-Allow-Origin", req.headers.origin);
+        // res.header("Access-Control-Allow-Origin", '*');
+        res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+        res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+        res.header("Access-Control-Allow-Credentials","true");
+        res.header("X-Powered-By",' 3.2.1')
+        if(req.method === "OPTIONS") res.send(200);/*让options请求快速返回*/
+        else  next();
+    });
     app.use(express.static('static'));
     app.use(express.static('node_modules/quill/dist'));
     app.use(bodyParser.urlencoded({ extended: false }));
